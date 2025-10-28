b262d3811ae73386fbf968b8e48f11daf1cd55ec
JaelleHaefliger <jaelle.haefliger@students.unibe.ch>
2025-10-28 09:18:01 +0100

    update plot

--- Plots for project.R
+++ Plots for project.R
@@ -1,10 +1,10 @@
 # code for plotting the result of different populations in one plot
-init_wd <- 500
-init_wb <- 50
-init_sd <- 500
-init_sb <- 50
-m_rate_wm <- 0.0007
-m_rate_sm <- 0.001
+init_wd <- 100
+init_wb <- 0
+init_sd <- 100
+init_sb <- 0
+m_rate_wm <- 0.003
+m_rate_sm <- 0.005
 decay_rate_wm <- 0.1
 decay_rate_sm <- 0.2
 s_wm <- 0.1
@@ -13,16 +13,15 @@ max_gen <- 1000
 replicates <- 50
 
 
-
+# defining Data Frame to save one Row (Population Size for multiple Generations until max_gen) after one Loop
 temp_output <- data.frame(array(NA, dim = c(0,max_gen)))
-
+# defining Data Frame to save all Results from Loop
 final_output <- data.frame(array(NA, dim = c(replicates,max_gen)))
+# defining a Set of colors in order to color each Line differently
 lines_colors <- rainbow(replicates)
+
 # simulating a population multiple times
 for (i in 1:replicates) {
-  
-  
-  
   temp_output <- rowSums(simulate_pop(
     init_wd, init_wb, init_sd, init_sb,
     decay_rate_wm, decay_rate_sm,
@@ -33,17 +32,32 @@ for (i in 1:replicates) {
   final_output[i, 1:length(temp_output)]<-temp_output
 }
 
-
+# defining end_gen which is as long as the last Generation where the pop size is an actual Number and not NA yet
 end_gen<-tail(which(!is.na(final_output[1,])), 1)
-plot(x=1:end_gen,y=final_output[1,1:end_gen],type='l',ylim=c(0,max(final_output, na.rm = TRUE)),xlab =  "Generation",ylab = "Population size", col = lines_colors[1])
-
+# Plot one Line / one Population
+plot(x=1:max_gen,y=final_output[1,1:max_gen],type='l',ylim=c(0,max(final_output, na.rm = TRUE)),xlab =  "Generation",ylab = "Population size", col = lines_colors[1])
+# Loop for plotting each Row of Data Table / Population as a Line in the Plot
 for (i in 2:replicates) {
   
   end_gen<-tail(which(!is.na(final_output[i,])), 1)
-  lines(x=1:end_gen,y=final_output[i,1:end_gen], col=lines_colors[i])
+  lines(x=1:max_gen,y=final_output[i,1:max_gen], col=lines_colors[i])
   
   
 }
+
+#init_wd <- 50
+#init_wb <- 50
+#init_sd <- 50
+#init_sb <- 50
+#m_rate_wm <- 0.005
+#m_rate_sm <- 0.007
+#decay_rate_wm <- 0.1
+#decay_rate_sm <- 0.2
+#s_wm <- 0.1
+#s_sm <- 0.2
+#max_gen <- 1000
+#replicates <- 50
+
 final_output1 <- rbind(final_output, c(1:1000))
 final_output2 <- data.frame(t(final_output1))
 dev.off()
