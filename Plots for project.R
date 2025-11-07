ed2bb43695967f0a487db3b434e9567d34728048
JaelleHaefliger <jaelle.haefliger@students.unibe.ch>
2025-11-07 14:26:26 +0100

    update comments

--- Plots for project.R
+++ Plots for project.R
@@ -1,10 +1,10 @@
 # code for plotting the result of different populations in one plot
-init_wd <- 50
-init_wb <- 50
-init_sd <- 50
-init_sb <- 50
-m_rate_wm <- 0.002 # approximately 0.4
-m_rate_sm <- 0.003 # approximately 0.6
+init_wd <- 30
+init_wb <- 30
+init_sd <- 30
+init_sb <- 30
+m_rate_wm <- 0.003 # approximately 0.4
+m_rate_sm <- 0.005 # approximately 0.6
 decay_rate_wm <- 0.1
 decay_rate_sm <- 0.2
 s_wm <- 0.1
@@ -41,7 +41,7 @@ for (i in 1:replicates) {
 # defining end_gen which is as long as the last Generation where the pop size is an actual Number and not NA yet
 end_gen<-tail(which(!is.na(final_output[1,])), 1)
 # Plot one Line / one Population
-plot(x=1:max_gen,y=final_output[1,1:max_gen],type='l',ylim=c(0,max(1500, na.rm = TRUE)),xlab =  "Generation",ylab = "Population size", col = lines_colors[1])
+plot(x=1:max_gen,y=final_output[1,1:max_gen],type='l',ylim=c(0,max(1400, na.rm = TRUE)),xlab =  "Generation",ylab = "Population size", col = lines_colors[1])
 # Loop for plotting each Row of Data Table / Population as a Line in the Plot
 for (i in 1:replicates) {
   
@@ -63,8 +63,8 @@ init_wd <- 30
 init_wb <- 30
 init_sd <- 30
 init_sb <- 30
-m_rate_wm <- 0.002 # approximately 0.4
-m_rate_sm <- 0.003 # approximately 0.6
+m_rate_wm <- 0.002 # approximately 0.2
+m_rate_sm <- 0.003 # approximately 0.3
 decay_rate_wm <- 0.1
 decay_rate_sm <- 0.2
 s_wm <- 0.1
@@ -136,16 +136,7 @@ for (i in 2:replicates) {
 #max_gen <- 1000
 #replicates <- 30
 
-final_output1 <- rbind(final_output, c(1:1000))
-final_output2 <- data.frame(t(final_output1))
-dev.off()
-dev.off()
-dev.off()
-plot(final_output2$X1~final_output2$X11, type = "l") 
-for (i in 2:replicates) {
-  lines(final_output2[,i]~final_output2$X11, col="red",na.rm=TRUE)
-  
-}
+
 output <- simulate_pop(init_wd=30, init_wb=30,  init_sd=30, init_sb=30, decay_rate_wm=0.1, decay_rate_sm=0.2, s_wm=0.1, s_sm=0.2, m_rate_wm=0.003, m_rate_sm=0.005,max_gen)
 
 # show the last few Lines of the Data Table
@@ -186,36 +177,38 @@ cat("\nProzentuale Häufigkeiten der Wechsel:\n")
 print(switch_stats)
 
 
-# Plotting one population
 output <- simulate_pop(init_wd=50, init_wb=50,  init_sd=50, init_sb=50, decay_rate_wm=0.1, decay_rate_sm=0.2, s_wm=0.1, s_sm=0.2, m_rate_wm=0.002, m_rate_sm=0.003,max_gen)
 
 # show the last few Lines of the Data Table
 
 print(tail(output))
 
-# plot the Output 
-# determine x axis Range 
+# extractin Population-Matrix 
+pop_matrix <- output$population
 
-x_range <- 0:(length(output[,1])-1)
+# assign frequencies of mutation changes
+switch_stats <- output$switch_stats
+switch_stats_all
 
+# determine x Axis Range 
+x_range <- 0:(nrow(pop_matrix)-1)
+
+# plot the Output
 # this plots the total Population Size-black line visible in the plot
+plot(x_range, pop_matrix[,1] + pop_matrix[,2] + pop_matrix[,3] + pop_matrix[,4],
+     type='l', xlim = c(0,max(1000, na.rm = TRUE)), ylim=c(0,max(500, na.rm = TRUE)), xlab="Generation", ylab="Population size",
+     col="black", lwd=1.5)
 
-plot(x_range,output[,1]+output[,2]+output[ ,3]+output[ ,4],type='l',ylim=c(0,max(output[,1]+output[,2]+output[ ,3]+output[ ,4])),xlab =  "Generation",ylab = "Population size")
+# adding each mutant
+lines(x_range, pop_matrix[,1], col="#77b5fe", lwd=1.5) # wd
+lines(x_range, pop_matrix[,2], col="navyblue", lwd=1.5) # wb
+lines(x_range, pop_matrix[,3], col="#D55E00", lwd=1.5) # sd
+lines(x_range, pop_matrix[,4], col="darkred", lwd=1.5) # sb
 
-# add Number of wd Individuals
-lines(x_range,output[,1], col="#77b5fe")
-
-# add Number of wb Individuals
-lines(x_range,output[,2], col="navyblue")
-
-# add Number of sd Individuals
-lines(x_range,output[,3], col="#D55E00")
-
-# add Number of sb Individuals
-lines(x_range,output[,4], col="darkred")
-
-# add Legend in the top-left corner
+# Legend
 legend("topleft",
-       legend=c("Weak deleterious","Weak beneficial","Strong deleterious","Strong beneficial"),
-       col=c("#77b5fe","navyblue","#D55E00","darkred"), lty=1, lwd=1.5)
+       legend=c("Total","wd","wb","sd","sb"),
+       col=c("black","#77b5fe","navyblue","#D55E00","darkred"),
+       lty=1, lwd=1.5)
+
 
